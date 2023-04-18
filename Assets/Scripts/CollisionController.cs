e05eb4087110fefbd53d26e19d1b4d8744137f27
Joann <59599987+joannhsiao@users.noreply.github.com>
2023-04-18 22:12:31 +0800

    Update CollisionController.cs

---
+++ Assets/Scripts/CollisionController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionController : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+    void OnCollisionEnter(Collision other)
+    {
+
+    }
+}
