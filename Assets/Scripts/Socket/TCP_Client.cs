8ce92e650f7b421d3e2d691a06542a387723640d
a0950088 <a0950088@gmail.com>
2023-05-23 17:59:34 +0800

    modified socket to synchronize

--- Assets/Scripts/Socket/TCP_Client.cs
+++ Assets/Scripts/Socket/TCP_Client.cs
@@ -5,6 +5,7 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 [Serializable]
 public class RecData{
@@ -15,49 +16,89 @@ public class RecData{
 
 public class TCP_Client : MonoBehaviour
 {
+    private Thread receiveThread;
     private TcpClient client;
     private NetworkStream stream;
     private byte[] receiveBuffer = new byte[1024];
-
-    private VideoController videoController;
+    private bool isRunning = true;
+    public String processPath;
     private bool isPath = false;
     private PlayerController player;
+
+    private void Awake() {
+        Debug.Log("Socket Awake");
+        IPAddress localIpAddress = IPAddress.Parse("127.0.0.1");//192.168.100.166
+        client = new TcpClient();
+        client.Client.Bind(new IPEndPoint(localIpAddress, 14786));
+        client.Connect("127.0.0.1", 30000);
+        
+
+        player = GameObject.FindObjectOfType<PlayerController>();
+        processPath = null;
+    }
 
     private void Start()
     {
         Debug.Log("Socket Start");
-        IPAddress localIpAddress = IPAddress.Parse("192.168.100.166");
-        client = new TcpClient();
-        client.Client.Bind(new IPEndPoint(localIpAddress, 14786));
-        client.Connect("192.168.100.166", 30000);
+        receiveThread = new Thread(SocketClientThread);
+        receiveThread.Start();
+        // sendThread = new Thread(SendData);
+        // sendThread.Start();
+        // IPAddress localIpAddress = IPAddress.Parse("192.168.100.166");
+        // client = new TcpClient();
+        // client.Client.Bind(new IPEndPoint(localIpAddress, 14786));
+        // client.Connect("192.168.100.166", 30000);
+        // stream = client.GetStream();
+
+        // videoController = GameObject.FindObjectOfType<VideoController>();
+        // player = GameObject.FindObjectOfType<PlayerController>();
+        // StartReceiving();
+    }
+
+    // private void Update()
+    // {
+    //     Debug.Log("Socket update");
+    //     // Send data to the server every second
+    //     if (Time.time >= 1f)
+    //     {
+    //         if (isPath)
+    //         {
+    //             SendData(videoController.videoFilePath);
+    //             isPath = false;
+    //         }
+    //         else
+    //         {
+    //             SendData("path: " + isPath);
+    //             // Debug.Log("Socket log");
+    //         }
+    //         Time.timeScale = 0f; // Pause the game after sending the data
+    //         StartReceiving();
+    //     }
+    // }
+    void SocketClientThread(){
         stream = client.GetStream();
-
-        videoController = GameObject.FindObjectOfType<VideoController>();
-        player = GameObject.FindObjectOfType<PlayerController>();
-        StartReceiving();
-    }
-
-    private void Update()
-    {
-        Debug.Log("Socket update");
-        // Send data to the server every second
-        if (Time.time >= 1f)
+        // StartReceiving();
+        int bytesRead;
+        string receivedData;
+        while (isRunning)
         {
-            if (isPath)
-            {
-                SendData(videoController.videoFilePath);
-                isPath = false;
-            }
-            else
-            {
-                SendData("path: " + isPath);
-                // Debug.Log("Socket log");
-            }
-            Time.timeScale = 0f; // Pause the game after sending the data
-            StartReceiving();
+            // 接收數據
+            // StartReceiving();
+            SendData();
+            bytesRead = stream.Read(receiveBuffer, 0, receiveBuffer.Length);
+            receivedData = Encoding.UTF8.GetString(receiveBuffer, 0, bytesRead);
+            ProcessRecData(receivedData);
+            // int bytesRead = stream.Read(receiveBuffer, 0, receiveBuffer.Length);
+            // if(bytesRead > 0){
+            //     string receivedData = Encoding.UTF8.GetString(receiveBuffer, 0, bytesRead);
+            //     ProcessRecData(receivedData);
+            // }
+            // stream.Read(receiveBuffer, 0, receiveBuffer.Length);
         }
+        stream.Close();
+        client.Close();
     }
-
+/* 非同步socket
     private void StartReceiving()
     {
         // Asynchronously receive data from the server
@@ -70,10 +111,17 @@ public class TCP_Client : MonoBehaviour
         {
             int bytesRead = stream.EndRead(result);
 
+            Debug.Log("bytesRead: "+bytesRead);
+
             if (bytesRead > 0)
             {
                 string receivedData = Encoding.ASCII.GetString(receiveBuffer, 0, bytesRead);             
+                Debug.Log(receivedData);
+                
+                    // Use the received data in the main Unity thread
                 ProcessRecData(receivedData);
+                    // ProcessReceivedData(receivedMessage);
+                
                 
                 // float.Parse(receivedData)
                 // SendData("Received data.");
@@ -82,10 +130,10 @@ public class TCP_Client : MonoBehaviour
         }
         catch (Exception e)
         {
-            Debug.LogError("Error receiving data: " + e.Message);
+            Debug.LogError("Error receiving data! "+ e.Message);
         }
     }
-
+*/
     private void ProcessRecData(string jsonData)
     {
         try
@@ -99,9 +147,9 @@ public class TCP_Client : MonoBehaviour
             float angle = dataObject.angle;
 
             // Process or use the received data as needed
-            Debug.Log("Speed: " + speed);
-            Debug.Log("Cadence: " + cadence);
-            Debug.Log("Angle: " + angle);
+            Debug.Log("Return Speed: " + speed);
+            Debug.Log("Return Cadence: " + cadence);
+            Debug.Log("Return Angle: " + angle);
             player.getSensorData(speed, cadence, angle);
         }
         catch (Exception)
@@ -110,15 +158,21 @@ public class TCP_Client : MonoBehaviour
         }
     }
 
-    private void SendData(string data)
+    private void SendData()
     {
-        byte[] sendData = Encoding.ASCII.GetBytes(data);
+        byte[] sendData;
+        if(processPath == null){
+            sendData = Encoding.ASCII.GetBytes("");
+        }
+        else{
+            sendData = Encoding.ASCII.GetBytes(processPath);
+        }
 
         try
         {
             stream.Write(sendData, 0, sendData.Length);
             stream.Flush();
-            Debug.Log("Sent data: " + data);
+            // Debug.Log("Sent data: " + msg);
         }
         catch (Exception e)
         {
@@ -129,6 +183,16 @@ public class TCP_Client : MonoBehaviour
     private void OnDestroy()
     {
         print("Destroy..");
+        isRunning = false;
+        receiveThread.Abort();
+        if (receiveThread != null && receiveThread.IsAlive)
+        {
+            receiveThread.Join();
+        }
+    }
+    private void OnApplicationQuit(){
+        isRunning = false;
+        receiveThread.Abort();
         if (client != null)
         {
             stream.Close();
