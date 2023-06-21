9769d8210386ee7ef7e0a2828c410545ea161325
yukiyuki333 <713883441ohya@gmail.com>
2023-06-21 23:57:41 +0800

    Count down before play.

--- Assets/Scripts/Loading_scene_ctrl.cs
+++ Assets/Scripts/Loading_scene_ctrl.cs
@@ -4,27 +4,23 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.IO;
+using System;
+using TMPro;
 
 public class Loading_scene_ctrl : MonoBehaviour
 {
     public GameObject load;
     public static string buttonName;
+    private TMP_Text str1;
     public void Start()
     {
-        if(TCP_Client.conn_state==1){
-            load.SetActive(false);
-            Debug.Log("Connected");
-            LoadScene("PlayScene");
-        }
-        else{
-            load.SetActive(true);
-            Debug.Log("Nothing");
-        }
+        load.SetActive(true);
+        str1=GameObject.Find("Loading_text").GetComponent<TMP_Text>();
     }
     public void Update()
     {
         if(TCP_Client.conn_state==1){
-            load.SetActive(false);
+            //load.SetActive(false);
             Debug.Log("Connected");
             LoadScene("PlayScene");
         }
@@ -47,7 +43,17 @@ public class Loading_scene_ctrl : MonoBehaviour
         // string videoPath = PlayerPrefs.GetString("VideoPath");
         string getPath = PlayerPrefs.GetString("VideoPath");
         Debug.Log("VideoPath: " + getPath);
+        StartCoroutine(WaitAndContinue(sceneName));
+    }
+    IEnumerator WaitAndContinue(string sceneName)
+    {
+        str1.text="3";
+        yield return new WaitForSecondsRealtime(1);
+        str1.text="2";
+        yield return new WaitForSecondsRealtime(1);
+        str1.text="1";
+        yield return new WaitForSecondsRealtime(1);
+        Debug.Log("Just Check");
         SceneManager.LoadScene(sceneName);
     }
-    
 }
