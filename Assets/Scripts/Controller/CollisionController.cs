2c393743d36a82328fe5efd86a1995a338870682
yukiyuki333 <713883441ohya@gmail.com>
2023-07-12 13:13:42 +0800

    Add SE and change hint size

--- Assets/Scripts/Controller/CollisionController.cs
+++ Assets/Scripts/Controller/CollisionController.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Globalization;
 
 public class CollisionController : MonoBehaviour
 {
     private Danger_jumpin_message danger_win;
     private int crash_num=0;
+    public AudioSource crashSE;
     void Start()
     {
         danger_win = GameObject.FindObjectOfType<Danger_jumpin_message>();
@@ -15,6 +17,10 @@ public class CollisionController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("obstacle")){
             print("!!!!!crushed obstacle.");
+            string SEString=PlayerPrefs.GetString("SEValue");
+            float SEnum=float.Parse(SEString, CultureInfo.InvariantCulture.NumberFormat);
+            crashSE.volume=SEnum;
+            crashSE.Play();
             danger_win.win_on = true;
             crash_num+=1;
             PlayerPrefs.SetString("crash_num", crash_num.ToString());
