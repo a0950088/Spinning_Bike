d48256ff026c0aa69202c76226687cef3a4eadf0
ShiraharaRaku <galanthus0101@gmail.com>
2023-07-23 03:12:03 +0800

    change rule picture

--- Assets/Scripts/Rule_button_fun/Last_button.cs
+++ Assets/Scripts/Rule_button_fun/Last_button.cs
@@ -4,26 +4,39 @@ using UnityEngine;
 
 public class Last_button : MonoBehaviour
 {
-    public GameObject rule1;
-    public GameObject rule2;
-    public GameObject rule3;
-    public GameObject rule4;
+    public GameObject rule1_photo;
+    public GameObject rule2_photo;
+    public GameObject rule3_photo;
+    public GameObject rule4_photo;
+
+    public GameObject rule1_text;
+    public GameObject rule2_text;
+    public GameObject rule3_text;
+    public GameObject rule4_text;
+
     public GameObject last_arrow;
     public GameObject next_arrow;
+
     public void On_Click_last(){
         Next_button.Which_rule--;
         if(Next_button.Which_rule==1){
-            rule2.SetActive(false);
-            rule1.SetActive(true);
+            rule2_photo.SetActive(false);
+            rule1_photo.SetActive(true);
+            rule2_text.SetActive(false);
+            rule1_text.SetActive(true);
             last_arrow.SetActive(false);
         }
         else if(Next_button.Which_rule==2){
-            rule3.SetActive(false);
-            rule2.SetActive(true);
+            rule3_photo.SetActive(false);
+            rule2_photo.SetActive(true);
+            rule3_text.SetActive(false);
+            rule2_text.SetActive(true);
         }
         else if(Next_button.Which_rule==3){
-            rule4.SetActive(false);
-            rule3.SetActive(true);
+            rule4_photo.SetActive(false);
+            rule3_photo.SetActive(true);
+            rule4_text.SetActive(false);
+            rule3_text.SetActive(true);
             next_arrow.SetActive(true);
         }
     }
--- Assets/Scripts/Rule_button_fun/Next_button.cs
+++ Assets/Scripts/Rule_button_fun/Next_button.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 
 public class Next_button : MonoBehaviour
 {
-    public GameObject rule1;
-    public GameObject rule2;
-    public GameObject rule3;
-    public GameObject rule4;
+    public GameObject rule1_photo;
+    public GameObject rule2_photo;
+    public GameObject rule3_photo;
+    public GameObject rule4_photo;
+
+    public GameObject rule1_text;
+    public GameObject rule2_text;
+    public GameObject rule3_text;
+    public GameObject rule4_text;
+
     public GameObject next_arrow;
     public GameObject last_arrow;
     public static int Which_rule=1;
+
     public void On_Click_next(){
         Which_rule++;
         if(Which_rule==2){
-            rule1.SetActive(false);
-            rule2.SetActive(true);
+            rule1_photo.SetActive(false);
+            rule2_photo.SetActive(true);
+            rule1_text.SetActive(false);
+            rule2_text.SetActive(true);
             last_arrow.SetActive(true);
         }
         else if(Which_rule==3){
-            rule2.SetActive(false);
-            rule3.SetActive(true);
+            rule2_photo.SetActive(false);
+            rule3_photo.SetActive(true);
+            rule2_text.SetActive(false);
+            rule3_text.SetActive(true);
         }
         else if(Which_rule==4){
-            rule3.SetActive(false);
-            rule4.SetActive(true);
+            rule3_photo.SetActive(false);
+            rule4_photo.SetActive(true);
+            rule3_text.SetActive(false);
+            rule4_text.SetActive(true);
             next_arrow.SetActive(false);
         }
     }
